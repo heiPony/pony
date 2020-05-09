f80c57f3374a9eaa4f0ecc4c2bb4c638c1aaa944
heiPony <1083365468@qq.com>
2020-05-09 11:21:03 +0800

    1

--- src/main/java/com/pony/test/TestApplication.java
+++ src/main/java/com/pony/test/TestApplication.java
@@ -1,6 +1,5 @@
 package com.pony.test;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
